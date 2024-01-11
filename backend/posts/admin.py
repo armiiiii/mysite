b81f5816c42cee05021e64ffa4714317b4c241c8
Igor Glushko <superigorek74@gmail.com>
2024-01-12 04:21:42 +0500

    Backend: Models update

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("name", "date_created")
+    search_fields = ("name", "id")
+
+admin.site.register(Post, PostAdmin)
--- backend/posts/models.py
+++ backend/posts/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+from django.utils.timezone import now
+
 from rest_framework import serializers
 
 class Post(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=50)
     content = models.TextField()
+    
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
