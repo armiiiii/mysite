16e34455ab26be6ee2d6f6c119f613e0c343ea96
Igor Glushko <superigorek74@gmail.com>
2024-01-11 05:21:04 +0500

    Services list updated

--- frontend/src/services.js
+++ frontend/src/services.js
@@ -1,10 +1,17 @@
+import axios from 'axios';
+
+async function fetch(url) {
+    const response = await axios.get(url);
+    return response.data
+};
+
 const services = {
-    fetch: (url) => {
-
+    async fetchPosts () {
+        return await fetch(`http://localhost:8000/posts`);
     },
-
-    fetchPosts: () => {
-        fetch(``)
+    async fetchById (id) {
+        return await fetch(`http://localhost:8000/posts/${id}`);
     },
+};
 
-}
+export default services;
