26e378e3b8fd8fb2c58418598c8191606ea1be44
Igor Glushko <superigorek74@gmail.com>
2024-06-09 00:21:54 +0500

    Deleted backend

--- backend/posts/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-
-from .models import Post, PostSerializer
-
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
